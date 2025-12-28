8015f20bb20786cd46ecfaccd177a99b3e56510d
Osama Abdullah Bin Salim <160643549+osamaaabdullah@users.noreply.github.com>
2025-12-28 13:07:23 -0500

    add vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import About from './pages/About'
 import Home from './pages/Home'
 import Footer from './components/Footer'
 import NavBar from './components/NavBar'
+import { Analytics } from "@vercel/analytics/next"
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
         </Routes>
         <Footer/>
       </BrowserRouter>
+      <Analytics/>
     </div>
   )
 }
