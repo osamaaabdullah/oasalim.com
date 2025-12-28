9e8fe596387760ec31ea3f266e7378aa8fe36a07
Osama Abdullah Bin Salim <160643549+osamaaabdullah@users.noreply.github.com>
2025-12-28 12:00:34 -0500

    add navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,19 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import About from './pages/About'
 import Home from './pages/Home'
 import Footer from './components/Footer'
+import NavBar from './components/NavBar'
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
+        <NavBar/>
         <Routes>
           <Route path="/" element={<Home />}/>
           <Route path ="/about" element={<About />}/>
         </Routes>
+        <Footer/>
       </BrowserRouter>
-      <Footer/>
     </>
   )
 }
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,59 @@
+import { useState } from "react"
+import { NavLink } from "react-router-dom"
+import { XIcon, MenuIcon } from "lucide-react"
+
+interface NavLinkType {
+    name: string
+    path: string
+}
+
+const navLinks: NavLinkType[] = [
+    {name: 'Home', path: '/'},
+    {name: 'About', path: '/about'}
+]
+
+const NavBar = () => {
+    const[isOpen, setIsOpen] = useState(false);
+    const handleClick = () => {
+        setIsOpen(!isOpen);
+    }
+
+    return (
+        <header className="bg-white rounded-2xl p-2 mx-auto mb-8">
+            <nav className="flex flex-wrap relative">
+                
+                <ul className="lg:flex hidden gap-3">
+                    {navLinks.map((link) => (
+                        <li key = {link.name}>
+                            <NavLink to={link.path}>
+                                {link.name}
+                            </NavLink>
+                        </li>
+                    ))}
+                </ul>
+                
+                {isOpen ? 
+                <div className="h-svh w-svw fixed left-0 top-0 backdrop-blur-sm" onClick={handleClick}></div>
+                :
+                null
+                }
+                <button className="m-0 my-auto lg:hidden z-10 " onClick={handleClick}>{isOpen? <XIcon/> : <MenuIcon/>}</button>
+                {isOpen ? <div className="bg-white w-1/2 absolute top-10 rounded-2xl lg:hidden">
+                    <ul className="my-4">
+                    {navLinks.map((link) => (
+                        <li key = {link.name} onClick={handleClick} className="m-2">
+                            <NavLink to={link.path}>
+                                {link.name}
+                            </NavLink>
+                        </li>
+                    ))}
+                    </ul>
+                </div>
+                : 
+                null}
+            </nav>
+        </header>
+    )
+}
+
+export default NavBar
