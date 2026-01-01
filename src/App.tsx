2155deee3c2bd771c09d41ea5ce473c71f6b3fc3
Osama Abdullah Bin Salim <160643549+osamaaabdullah@users.noreply.github.com>
2025-12-31 23:47:33 -0500

    fix: small screen responsiveness and make light the default theme

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { Analytics } from "@vercel/analytics/react"
 import { useEffect, useState } from 'react'
 
 const App = () => {
-  const [isDark, setIsDark] = useState(true);
+  const [isDark, setIsDark] = useState(false);
 
   useEffect(() => {
   const root = document.documentElement;
@@ -20,7 +20,7 @@ const App = () => {
   }
 
   return (
-    <div className='md:w-9/10 lg:w-8/10 max-[1100px]:w-[70%] mx-auto text-center'>
+    <div className='md:w-9/10 lg:w-8/10 min-[1100px]:w-[70%] mx-auto text-center'>
       <BrowserRouter>
         <Analytics/>
         <NavBar isDark={isDark} handleChange={handleChange}/>
--- src/components/ThemeToggleButton.tsx
+++ src/components/ThemeToggleButton.tsx
@@ -14,8 +14,8 @@ const ThemeToggleButton = ({ isDark, handleChange }: ThemeToggleButtonProps) => 
   
   return (
     <div className="border-solid bg-[#EEEEEE] dark:bg-[#242424] w-fit rounded-2xl relative">
-      <button className={`${!isOpen ? "opacity-0" : "opacity-100"} py-1 px-2 rounded-l-2xl`} onClick={() =>{handleChange(); handleClick();}} aria-pressed={isDark}><SunMedium className={`${!isOpen ? "opacity-0" : "opacity-100"} text-amber-300 fill-amber-300 z-10`}/></button>
-      <button className= {`${isOpen ? "opacity-0" : "opacity-100"} py-1 px-2 rounded-r-2xl`} onClick={() =>{handleChange(); handleClick();}} aria-pressed={isDark}><Moon className={`${isOpen ? "opacity-0" : "opacity-100"} text-gray-700 fill-gray-700 z-10`}/></button>
+      <button className={`${isOpen ? "opacity-0" : "opacity-100"} py-1 px-2 rounded-l-2xl`} onClick={() =>{handleChange(); handleClick();}} aria-pressed={isDark}><SunMedium className={`${isOpen ? "opacity-0" : "opacity-100"} text-amber-300 fill-amber-300 z-10`}/></button>
+      <button className= {`${!isOpen ? "opacity-0" : "opacity-100"} py-1 px-2 rounded-r-2xl`} onClick={() =>{handleChange(); handleClick();}} aria-pressed={isDark}><Moon className={`${!isOpen ? "opacity-0" : "opacity-100"} text-gray-700 fill-gray-700 z-10`}/></button>
       {/* <div className="bg-black inset-0 rounded-full absolute w-1/3 m-1"></div> */}
     </div>
   )
