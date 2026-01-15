48f067003daf9df7d56978581ab1ea98ab86a86e
Osama Abdullah Bin Salim <160643549+osamaaabdullah@users.noreply.github.com>
2026-01-15 10:13:15 -0500

    backend: add post, get, put, delete routes for blog posts

--- backend/app.js
+++ backend/app.js
@@ -6,20 +6,66 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
-
+app.use(express.urlencoded({extended: false}));
 
 app.get('/', (req, res) => {
   res.send('Welcome to my blog!');
-})
+});
+
+app.get('/api/posts', async (req, res) => {
+  try {
+    const posts = await Post.find({});
+    res.status(200).json(posts);
+  } catch (error) {
+    res.status(500).json({message: error.message});
+  }
+});
+
+app.get('/api/post/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Post.findById(id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({message: error.message});
+  }
+});
 
 app.post('/api/post', async (req, res) => {
   try {
     const post = await Post.create(req.body);
     res.status(200).json(post);
   } catch (error) {
-    res.status(500).json({message: error.message})
+    res.status(500).json({message: error.message});
   }
-})
+});
+
+app.put('api/post/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const post = await Post.findByIdAndUpdate(id, req.body);
+    if (!post) {
+      return res.status(400).json({message: "Post not found"});
+    }
+    const updatedPost = await Post.findById(id);
+    res.status(200).json(updatedPost);
+  } catch (error) {
+    res.status(500).json({message: error.message});
+  }
+});
+
+app.delete('/api/post/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const post = await Post.findByIdAndDelete(id);
+    if(!post) {
+      return res.status(400).json({message: "Post doesn't exist"});
+    }
+    res.status(200).json({message: "Post deleted"});
+  } catch (error) {
+    res.status(500).json({message: error.message});
+  }
+});
 
 mongoose.connect(process.env.DATABASE_URI).then(
   () => {
@@ -32,4 +78,4 @@ mongoose.connect(process.env.DATABASE_URI).then(
   () => {
     console.log("Connection failed")
   }
-)
+);
