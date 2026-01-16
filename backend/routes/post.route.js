93d33fd80f2b2bf2d2e74e931440b835128f7961
Osama Abdullah Bin Salim <160643549+osamaaabdullah@users.noreply.github.com>
2026-01-16 14:59:53 -0500

    backend: protect endpoints

--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const {getPostById, postPost, updatePost, deletePost} = require('../controller/post.controller.js')
+const {authMiddleware} = require('../middleware/auth.middleware.js');
 
 router.get('/:id', getPostById);
-router.post('/', postPost);
-router.put('/:id', updatePost);
-router.delete('/:id', deletePost);
+router.post('/', authMiddleware, postPost);
+router.put('/:id', authMiddleware, updatePost);
+router.delete('/:id', authMiddleware, deletePost);
 
 module.exports = router;
