0c49df9e8142db545e4d2c3142a715dbf6a4d0a4
Osama Abdullah Bin Salim <160643549+osamaaabdullah@users.noreply.github.com>
2026-01-17 20:35:47 -0500

    backend: Enable cors

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,17 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const postsRoute = require('./routes/posts.route.js');
 const postRoutes = require('./routes/post.route.js');
 const authRoutes = require('./routes/auth.route.js');
 const app = express();
 const port = process.env.PORT;
+
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
