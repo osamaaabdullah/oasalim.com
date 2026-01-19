4354809748f3acaeb1f459511d18e1b0a986ddbb
Osama Abdullah Bin Salim <160643549+osamaaabdullah@users.noreply.github.com>
2026-01-19 16:33:25 -0500

    backend: add image url to Post Schema

--- backend/models/post.model.js
+++ backend/models/post.model.js
@@ -14,6 +14,10 @@ const PostSchema = mongoose.Schema(
         category: {
             type: String,
             required: false
+        },
+        image: {
+            type: String,
+            required: false
         }
     },
     {
