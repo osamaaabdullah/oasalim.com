b5bd151c8d7f6cd1f3fe77737b6854947547cac2
Osama Abdullah Bin Salim <160643549+osamaaabdullah@users.noreply.github.com>
2026-01-19 19:38:11 -0500

    frontend: configure API URL using env

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-const API_URL = "http://localhost:3000/api";
+const API_URL = import.meta.env.VITE_API_URL;
 
 interface Post {
     _id: string;
--- src/pages/BlogPost.tsx
+++ src/pages/BlogPost.tsx
@@ -13,7 +13,7 @@ interface Post {
     image: string;
 }
 
-const API_URL = "http://localhost:3000/api/post"
+const API_URL = import.meta.env.VITE_API_URL;
 
 const BlogPost = () => {
 
