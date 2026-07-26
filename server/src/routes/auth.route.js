8dd322316e7124e9753ebea6ffb76c84bacf279e
osamaaabdullah <oasalim13@gmail.com>
2026-07-26 16:48:16 -0400

    backend: disable signup route

--- server/src/routes/auth.route.js
+++ server/src/routes/auth.route.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { signUp, login } = require('../controllers/auth.controller.js')
 
-router.post('/signup', signUp);
+// uncomment to enable signup route
+// router.post('/signup', signUp);
 router.post('/login', login);
 
 module.exports = router;
